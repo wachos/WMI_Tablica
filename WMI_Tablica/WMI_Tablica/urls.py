4cd5776bdd022be7bbe3644b60d086a55d62c798
chevysniff <chewysniff@gmail.com>
2014-03-11 21:52:23 +0100

    Prototyp

--- WMI_Tablica/WMI_Tablica/urls.py
+++ WMI_Tablica/WMI_Tablica/urls.py
@@ -11,4 +11,8 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name='index'),
+    url(r'^edit_personal_data$', views.edit_personal_data, name='edit_personal_data'),
+    url(r'^watching_ads$', views.watching_ads, name='watching_ads'),
+    url(r'^published_ads$', views.published_ads, name='published_ads'),
+    url(r'^new_ad$', views.new_ad, name='new_ad'),
 )
--- WMI_Tablica/website/views.py
+++ WMI_Tablica/website/views.py
@@ -1,6 +1,46 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render_to_response('base.html', { 'text' : 'dupa123' })
+    ad = """
+    <div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title">Przykładowe ogłoszenie 1</h3>
+  </div>
+  <div class="panel-body">
+    <div class="ad-text">
+        <p>
+            <a href="#" class="thumbnail">
+                <img src="../static/images/1.jpg" alt="...">
+            </a>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </div>
+  </div>
+  <div class="panel-footer">
+    <div class="ad-date">Data dodania: <b>12-12-2012</b></div>
+    <div class="btn-group pull-right">
+      <button type="button" class="btn btn-default">Więcej</button>
+      <button type="button" class="btn btn-default">Obserwuj</button>
+    </div>
+  </div>
+</div>
+    """
+    ads_list = [ad, ad, ad]
+    return render_to_response('index.html', {'ads_list' : ads_list})
+
+
+def edit_personal_data(request):
+    return render_to_response('edit_personal_data.html')
+    
+def watching_ads(request):
+    return render_to_response('watching_ads.html')
+
+def published_ads(request):
+    return render_to_response('watching_ads.html')
+
+def new_ad(request):
+    return render_to_response('new_ad.html')
