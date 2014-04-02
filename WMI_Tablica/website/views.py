b06aa48016d94796d528685d1c51532a1c7f44a3
Tomek <tomek@tomek-Aspire-E1-531.(none)>
2014-04-02 18:26:04 +0200

    wyswietlanie ogloszen

--- WMI_Tablica/website/views.py
+++ WMI_Tablica/website/views.py
@@ -31,32 +31,8 @@ def logout_page(request):
     
 @login_required
 def index(request):
-    ad = """
-    <div class="panel panel-default">
-  <div class="panel-heading">
-    <h3 class="panel-title">Przykładowe ogłoszenie 1</h3>
-  </div>
-  <div class="panel-body">
-    <div class="ad-text">
-        <p>
-            <a href="#" class="thumbnail">
-                <img src="../static/images/1.jpg" alt="...">
-            </a>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-    </div>
-  </div>
-  <div class="panel-footer">
-    <div class="ad-date">Data dodania: <b>12-12-2012</b></div>
-    <div class="btn-group pull-right">
-      <button type="button" class="btn btn-default">Więcej</button>
-      <button type="button" class="btn btn-default">Obserwuj</button>
-    </div>
-  </div>
-</div>
-    """
-    ads_list = [ad, ad, ad]
-    return render_to_response('index.html', {'ads_list' : ads_list})
+    all_entries = Ogloszenie.objects.all().order_by('-id')
+    return render_to_response('index.html',{'pubb':all_entries})
 
 def edit_personal_data(request):
     return render_to_response('edit_personal_data.html')
